474c5508ef6eaf94023a5fbd81dc38379a2a45eb
Wesley Maffly-Kipp <maffkipp@yahoo.com>
2017-12-03 23:04:19 -0600

    added most of about component, lots of styles

--- src/components/About.js
+++ src/components/About.js
@@ -4,7 +4,20 @@ class About extends Component {
   render() {
     return (
       <div className='about'>
-        <h2 className='about-title'>About Component</h2>
+        <h2 className='about-title'>About Me</h2>
+        <div className='about-container'>
+          <div className='about-inner-flexbox'>
+            <img  className='about-image'
+                  src={require('../images/headshot.jpg')}
+                  alt='Portrait'/>
+            <p className='about-body'>I am a full stack web developer with
+             a flair for creativity and outside-of-the-box problem solving.
+             My experience as a musician has given me the ability to perform
+             well under pressure and improvise when the situation calls for
+             it, skills I put to use building powerful, elegant web apps.</p>
+          </div>
+          <h3 className='about-skills-title'>Skills:</h3>
+        </div>
       </div>
     )
   }
