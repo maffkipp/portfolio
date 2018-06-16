78a8680197c50ddb06e6d4327b116209fa3bceea
Wesley Maffly-Kipp <maffkipp@yahoo.com>
2018-06-15 23:16:16 -0500

    added weathervue project, removed podster

--- src/projectData.js
+++ src/projectData.js
@@ -2,7 +2,7 @@ import petesPedals from './images/petes-pedals.png';
 import bogbody from './images/bogbody.png';
 import myTravels from './images/my-travels.png';
 import ticTacToe from './images/tic-tac-toe.png';
-import podster from './images/podster.png';
+import weathervue from './images/weathervue.png';
 
 
 const projectData = [
@@ -12,7 +12,7 @@ const projectData = [
     image: petesPedals,
     url: "https://www.petespedals.net/",
     github: "https://github.com/maffkipp/petes_pedals",
-    about: "This is a website I built for my Dad's guitar pedal business. I was focused on making a site that could be easily modified and expanded by the client without them needing to edit any code.",
+    about: "This is a website I built for a client's guitar pedal business. I was focused on making a site that could be easily modified and expanded by the client without them needing to edit any code.",
     details: "This project was built using primarily React and Contentful CMS. This combination allowed me full control over the layout while still affording the client all the benefits of a modern CMS. I looked to Material Design for inspiration for the layout, colors, and fonts; I also used react-images for the photo lightbox and moment.js for date parsing."
   },
   {
@@ -25,8 +25,17 @@ const projectData = [
     details: "I used this project as an opportunity to dive into using webpack as a build tool, as I previously had not had the opportunity to use it outside the React ecosystem. I used babel for ES6 support, and I used Sass to write my stylesheets. I also used animate.css for simple animations along with smooth-scroll."
   },
   {
-    title: "My Travels",
+    title: "WeatherVue",
     id: "03",
+    image: weathervue,
+    url: "https://weathervue.wesmk.com/",
+    github: "https://github.com/maffkipp/vue-weather-app",
+    about: "This is a weather app I built using the OpenWeatherMap API. It was built with the intention of learning more about VueJS and the HTML5 Canvas API.",
+    details: "This project was built using vue-cli. It utilizes Vue to update the page based on the data pulled in from an external API. The background of the page is an HTML5 canvas element set to randomly create circles of different sizes, directions, and velocities; collisions with the outer edges of the browser window are also accounted for."
+  },
+  {
+    title: "My Travels",
+    id: "04",
     image: myTravels,
     url: "http://my-travels-log.herokuapp.com/",
     github: "https://github.com/maffkipp/my_travels_log",
@@ -35,21 +44,12 @@ const projectData = [
   },
   {
     title: "Tic Tac Toe",
-    id: "04",
+    id: "05",
     image: ticTacToe,
     url: "https://tic-tac-toe-maffkipp.herokuapp.com/",
     github: "https://github.com/maffkipp/tic-tac-toe/tree/wes_lab",
     about: "A game of tic-tac-toe built using HTML, CSS, and vanilla Javascript. This is an older project, but I really like the way it turned out visually. ",
     details: "Besides the basic front-end tools, I built a simple server for this app with NodeJS and Express. I also am using Sass on this project as a way to keep my CSS better organized. The primary goal of this project was to become comfortable with DOM manipulation without using Jquery, and I would say I was fairly successful in that regard."
-  },
-  {
-    title: "Podster",
-    id: "05",
-    image: podster,
-    url: "https://podster.mattray.io/",
-    github: "https://github.com/MattRay0295/podcast-app",
-    about: "This is a single page app that helps users find new podcasts by trending or by category; there is also a 'random podcast' button. Unfortunately, the API we used has since been shut down, and the app is currently not recieving any data.",
-    details: "I was part of a part of a three person group working on this project, but I contributed to most every part of it. I was solely responsible for the CSS and for setting up the proxy server with Node and Express, but I also spent a lot of time working with React to build all our front end logic. We used the Audiosearch API, which is now offline, but I have plans to rebuild the server down the line."
   }
 ]
 
